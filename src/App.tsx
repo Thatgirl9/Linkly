96c99d129995513b144abe042c98492e4b25fae2
Favour Ukonu <favouroukonu@gmail.com>
2024-02-12 21:13:18 +0100

    Chore: Linkly Capstone Start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <section>
+      <h1 className="bg-red-600 text-yellow-500">Linkly</h1>
+    </section>
+  );
+}
+
+export default App;
