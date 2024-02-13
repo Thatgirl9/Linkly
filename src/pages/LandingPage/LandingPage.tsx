707e7806de4b14b55096502b9a22aa20658b5cce
Favour Ukonu <favouroukonu@gmail.com>
2024-02-13 22:35:44 +0100

    Fix: Background image

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,13 +1,42 @@
 import "./landing.css";
-import bgImage from "../../assets/Group 17.png";
+// import bgImage from "../../assets/Group 17.png";
 
-const LandingPage = () => {
+const LandingPage: React.FC = () => {
   return (
-    <section
-      className=" landing-page h-screen border-red-700 border-4"
-      style={{ backgroundImage: `url(${bgImage})` }}
-    >
-      {/* <h1>Wassup! I'm here</h1> */}
+    <section className="landingPage  font-inter h-screen">
+      <div>
+        {/* <img src={bgImage} className="w-full " /> */}
+        {/* <div className="flex justify-between items-center">
+          <div className="flex items-center">
+            <img src={bgImage} alt="logo" className="w-[20em] h-[10em]" />
+            <h1 className="text-3xl font-bold text-white ml-2">Linkly</h1>
+          </div>
+          <div>
+            <button className="bg-white text-primaryPink text-sm font-bold px-4 py-2 rounded-lg">
+              Sign In
+            </button>
+          </div>
+        </div>
+        <div className="mt-20">
+          <h1 className="text-5xl font-bold text-white">
+            Shorten your link and track it.
+          </h1>
+          <p className="text-white mt-4">
+            A simple and easy to use tool to shorten your links, and track how
+            many people clicked on it.
+          </p>
+          <div className="flex mt-8">
+            <input
+              type="text"
+              placeholder="Shorten your link"
+              className="w-1/2 px-4 py-2 rounded-l-lg"
+            />
+            <button className="bg-secondaryPink text-white px-4 py-2 rounded-r-lg">
+              Shorten
+            </button>
+          </div>
+        </div> */}
+      </div>
     </section>
   );
 };
