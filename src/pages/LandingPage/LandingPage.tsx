24efed32afe4048ada5124f7d2bac7476ae971d0
Favour Ukonu <favouroukonu@gmail.com>
2024-02-15 23:47:35 +0100

    Feat: Hero section added

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -4,41 +4,20 @@ import "./landing.css";
 
 const LandingPage: React.FC = () => {
   return (
-    <section className="landingPage font-inter ">
+    <section className="landingPage font-inter border-2 border-red-400 text-white">
       <NavBar />
-
-      <div>
-        {/* <img src={bgImage} className="w-full " /> */}
-        {/* <div className="flex justify-between items-center">
-          <div className="flex items-center">
-            <img src={bgImage} alt="logo" className="w-[20em] h-[10em]" />
-            <h1 className="text-3xl font-bold text-white ml-2">Linkly</h1>
-          </div>
-          <div>
-            <button className="bg-white text-primaryPink text-sm font-bold px-4 py-2 rounded-lg">
-              Sign In
-            </button>
-          </div>
-        </div>
-        <div className="mt-20">
-          <h1 className="text-5xl font-bold text-white">
-            Shorten your link and track it.
+      <div className="hero pt-[8em] lg:pt-[10em] justify-center items-center flex">
+        <div className="hero-text text-center flex flex-col gap-2">
+          <h1 className="text-4xl lg:text-5xl  font-extrabold text-white hero__p">
+            Shorten Your Loooong Links :)
           </h1>
-          <p className="text-white mt-4">
-            A simple and easy to use tool to shorten your links, and track how
-            many people clicked on it.
+
+          <p className="text-primaryLite">
+            Linkly is an efficient and easy-to-use URL shortening service that
+            streamlines your<br className="hidden lg:block"></br> online
+            experience.
           </p>
-          <div className="flex mt-8">
-            <input
-              type="text"
-              placeholder="Shorten your link"
-              className="w-1/2 px-4 py-2 rounded-l-lg"
-            />
-            <button className="bg-secondaryPink text-white px-4 py-2 rounded-r-lg">
-              Shorten
-            </button>
-          </div>
-        </div> */}
+        </div>
       </div>
     </section>
   );
