b3dbaf76555812807185d77d3ccff2b3b3c75de2
Favour Ukonu <favouroukonu@gmail.com>
2024-02-19 21:14:37 +0100

    Feat: Toggle Switch completed, Assets added

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -18,7 +18,26 @@ const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ onChange }) => {
   return (
     <div className="flex items-center space-x-2">
       {/* Label and Input elements for the switch */}
-      <label></label>
+      <label className="cursor-pointer">
+        {/* Hidden checkbox input that is controlled by the component's state */}
+        <input
+          type="checkbox"
+          className="hidden"
+          checked={checked}
+          onChange={handleToggle}
+        />
+        {/* Styled div to represent the visual switch */}
+        <div className="w-12 h-6 bg-primaryGrey rounded-full p-1 transition duration-300 ease-in-out border border-stroke">
+          {/* White circle representing the handle of the switch */}
+          <div
+            className={` w-4 h-4 rounded-full shadow-md transform duration-300 ease-in-out ${
+              checked
+                ? "translate-x-6 bg-primaryBlue "
+                : "translate-x-0 bg-slate-300"
+            }`}
+          ></div>
+        </div>
+      </label>
     </div>
   );
 };
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -3,12 +3,18 @@ import NavBar from "../../components/Nav";
 import "./landing.css";
 // import bgImage from "../../assets/Group 17.png";
 import InputLink from "../../assets/link.png";
+import ToggleSwitch from "../../components/ToggleSwitch";
 
 const LandingPage: React.FC = () => {
+  const handleToggle = (checked: boolean) => {
+    console.log("Checked: ", checked);
+  };
+
   return (
     <section className="landingPage font-inter">
       <NavBar />
-      <div className="hero pt-[8em] lg:pt-[10em] justify-center items-center flex flex-col gap-[3.7em]">
+      <div className="hero pt-[8em] lg:pt-[10em] justify-center items-center flex flex-col gap-[3.7em] landing-page-div ">
+        {/* Texts */}
         <div className="hero-text text-center flex flex-col gap-2 w-[90%] md:w-[95%] lg:w-fit">
           <h1 className="text-4xl lg:text-5xl  font-extrabold text-white hero__p">
             Shorten Your Loooong Links :)
@@ -21,19 +27,28 @@ const LandingPage: React.FC = () => {
           </p>
         </div>
 
-        <div className="sm:w-[30em] w-[90%]  rounded-full flex items-center justify-center gap-2 bg-primaryGrey px-[0.3em] h-[3.3em] border-[3px] border-stroke">
-          <img
-            src={InputLink}
-            alt="Link Icon"
-            className="h-[1.2em] w-[2em] pl-[0.5em]"
-          />
-          <input
-            className="bg-transparent outline-none sm:w-[28em] w-[70%]"
-            placeholder="Enter the link here"
-          ></input>
-          <button className="bg-primaryBlue text-white font-semibold px-4 py-2 rounded-full shadow-2xl shadow-primaryBlue">
-            Shorten!
-          </button>
+        {/* Link Input, Toggle Switch and so.... */}
+        <div className="flex flex-col gap-[1.7em] justify-center items-center">
+          {/* Link Input */}
+          <div className="sm:w-[30em] w-[90%]  rounded-full flex items-center justify-center gap-2 bg-primaryGrey px-[0.3em] h-[3.3em] border-[3px] border-stroke">
+            <img
+              src={InputLink}
+              alt="Link Icon"
+              className="h-[1.2em] w-[2em] pl-[0.5em]"
+            />
+            <input
+              className="bg-transparent outline-none sm:w-[28em] w-[70%]"
+              placeholder="Enter the link here"
+            ></input>
+            <button className="bg-primaryBlue text-white font-semibold px-4 py-2 rounded-full shadow-2xl shadow-primaryBlue">
+              Shorten!
+            </button>
+          </div>
+
+          {/* Toggle switch and Texts  */}
+          <div>
+            <ToggleSwitch onChange={handleToggle} />
+          </div>
         </div>
       </div>
 
