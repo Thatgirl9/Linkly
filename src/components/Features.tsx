39659d71ac3609a25b3a008492812d1bbdd8ad4e
Favour Ukonu <favouroukonu@gmail.com>
2024-02-20 21:19:02 +0100

    Feat: Feature component complete

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,12 +1,50 @@
-import Reason4 from "../assets/FeaturesAssets/Frame 1000001695 (1).svg";
+import Reason3 from "../assets/FeaturesAssets/Frame 1000001695 (1).svg";
 import Reason2 from "../assets/FeaturesAssets/Frame 1000001695.svg";
-import Reason3 from "../assets/FeaturesAssets/Frame 1000001718.svg";
+import Reason4 from "../assets/FeaturesAssets/Frame 1000001718.svg";
 import Reason1 from "../assets/FeaturesAssets/Group 6.svg";
 
 const Features: React.FC = () => {
+  const ReasonsForLinkly = [
+    // 1st Reason
+    {
+      image: Reason1,
+      alt: "Link Icon",
+      title: "URL Shortening",
+      description:
+        "Linkly allows you to shorten URLs of your business, events. Shorten your URL at scale, URL redirects.",
+    },
+
+    // 2nd Reason
+    {
+      image: Reason2,
+      alt: "Editing Icon",
+      title: "Custom URLs",
+      description:
+        "With Linkly, you can create custom URLs, with the length you want! A solution for socials and businesses.",
+    },
+
+    // 3rd Reason
+    {
+      image: Reason3,
+      alt: "QR Codes Icon",
+      title: "QR Codes",
+      description:
+        " Generate QR codes to your business, events. Bring your audience and customers to your doorstep with this scan and go solution.",
+    },
+
+    // 4th Reason
+    {
+      image: Reason4,
+      alt: "Data Analytics Icon",
+      title: "Data Analytics",
+      description:
+        "Receive data on the usage of either your shortened URL, custom URLs or generated QR codes. Embedded to monitor progress.",
+    },
+  ];
+
   return (
     <section
-      className=" md:mt-[6em] mt-[5em] text-primaryLite flex justify-center items-center pb-[4em] "
+      className=" md:pt-[6em] pt-[5em] text-primaryLite flex md:flex-row flex-col justify-center items-center md:items-start gap-[4em] pb-[4em] scroll-m-16"
       id="features"
     >
       {/* Why choose Linkly */}
@@ -24,36 +62,22 @@ const Features: React.FC = () => {
       </div>
 
       {/* Reasons  */}
-      <div>
-        {/* 1st Reason */}
-        <div>
-          <div>
-            <img src={Reason1} alt="Link Icon" />
-          </div>
+      <div className="grid  lg:grid-cols-2 grid-cols-1 gap-[2em]">
+        {ReasonsForLinkly.map((reason, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-col gap-[1em] border border-primaryGrey rounded-xl p-4 hover:cursor-pointer hover:shadow-lg hover:shadow-primaryGrey"
+            >
+              <div>
+                <img src={reason.image} alt={reason.alt} />
+              </div>
 
-          <h3 className="font-semibold">URL Shortening</h3>
-        </div>
-
-        {/* 2nd Reason */}
-        <div>
-          <div>
-            <img src={Reason4} alt="" />
-          </div>
-        </div>
-
-        {/* 3rd Reason */}
-        <div>
-          <div>
-            <img src={Reason2} alt="Reason2" />
-          </div>
-        </div>
-
-        {/* 4th Reason */}
-        <div>
-          <div>
-            <img src={Reason3} alt="Reason3" />
-          </div>
-        </div>
+              <h3 className="font-semibold">{reason.title}</h3>
+              <p className="w-[33ch] text-sm">{reason.description}</p>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
