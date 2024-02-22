3ba9d2bf010c5bb94572dd88a9d42e982874109a
Favour Ukonu <favouroukonu@gmail.com>
2024-02-22 20:20:04 +0100

    Feat: Login Page

---
+++ src/pages/LoginPage/Login.tsx
@@ -0,0 +1,12 @@
+import "./login.css";
+
+const LoginPage = () => {
+  return (
+    <div>
+      Login Page babyyyyyyyyyyyyy
+      <h2>Wassssssupppp</h2>
+    </div>
+  );
+};
+
+export default LoginPage;
