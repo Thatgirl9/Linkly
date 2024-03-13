dca0f88e7cc3fffffd00bc03db788674c98d78cd
Favour Ukonu <favouroukonu@gmail.com>
2024-03-13 14:52:36 +0100

    Fix: SupabaseClient

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -4,5 +4,12 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_APP_ANON_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  supabaseUrl ?? '',
+  supabaseKey ?? '',
+  {
+    persistSession: true,
+    autoRefreshToken: true
+  }
+)
 
