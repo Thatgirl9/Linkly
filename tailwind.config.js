8daadbd38f97d7fdb31eb6d371699cd403574083
Favour Ukonu <favouroukonu@gmail.com>
2024-03-14 22:06:11 +0100

    Feat:Add dashboard asset

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ export default {
         primaryBlack: '#0B101B',
         primaryGrey: '#181E29',
         primaryLite: '#C9CED6',
+        primaryGreen: '#1EB036',
+        primaryBrown: '#B0901E',
         stroke: '#353C4A'
       },
       fontFamily: {
